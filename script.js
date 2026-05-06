74d00c36036957a5bb61325aa13bcdfd2048c16b
edjozero <ej21serafin@gmail.com>
2026-05-05 18:21:00 -0700

    Add functionality for the input and button to be a palindrome checker

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
+
+checkBtn.addEventListener('click', () => {
+    const text = textInput.value;
+
+    if(text === '') {
+        resultDiv.textContent = 'Please enter a string.';
+        return;
+    }
+    
+    // regex
+    const regex = text.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
+    const reversedText = regex.split('').reverse().join('');
+
+    // check if the regex is the same as the reversed text
+    if(regex === reversedText) {
+        resultDiv.textContent = `"${text}" is a palindrome.`;
+    } else {
+        resultDiv.textContent = `"${text}" is not a palindrome.`;
+    }
+
+    textInput.value = '';
+});
